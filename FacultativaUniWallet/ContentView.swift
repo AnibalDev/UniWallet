4ffcddd8d57f71b4195f77100df11201295da05a
AnibalDev <anibalrodriguezdeveloper@gmail.com>
2024-05-23 09:39:27 -0600

    Initial Commit

---
+++ FacultativaUniWallet/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FacultativaUniWallet
+//
+//  Created by Anibal Rodriguez on 23/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FacultativaUniWallet/FacultativaUniWalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  FacultativaUniWalletApp.swift
+//  FacultativaUniWallet
+//
+//  Created by Anibal Rodriguez on 23/5/24.
+//
+
+import SwiftUI
+
+@main
+struct FacultativaUniWalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
